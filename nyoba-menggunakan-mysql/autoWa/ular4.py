9abbb6491eff79d1c89ec6e68946fba17c8835ab
yasashi23 <106567885+yasashi23@users.noreply.github.com>
2023-02-03 07:00:15 +0700

    new

--- nyoba-menggunakan-mysql/autoWa/ular4.py
+++ nyoba-menggunakan-mysql/autoWa/ular4.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+import pyperclip
+import keyboard
 import time
 
 
@@ -26,23 +28,23 @@ print("=============== BERJALAN 2 ==============")
 
 target = '"No.2bisnis"'
 contact_path = '//span[contains(@title,' + target + ')]'
-contact = WebDriverWait(browser, 500).until(
+contact = WebDriverWait(browser, 100).until(
     EC.presence_of_all_elements_located((By.XPATH, contact_path)))
 contact[0].click()
 print("=============== BERJALAN 3 ==============")
-
-
 time.sleep(1)
 text = "udin"
+keyboard.write("bukannya diri ini")
 
-classnya = "_3Uu1_"
-input = '//div[contains(@class,' + classnya + ')]'
-input_wait = WebDriverWait(browser, 500).until(
+input = "//div[@class='_3Uu1_']"
+input_wait = WebDriverWait(browser, 300).until(
     EC.presence_of_all_elements_located((By.XPATH, input)))
-inputnya = browser.find_element("xpath",input)
-input_wait[0].click()
+# input = browser.find_element(By.CLASS_NAME, "fd365im1 to2l77zo bbv8nyr4 mwp4sxku gfz4du6o ag5g9lrv bze30y65 bdf91cm1")
+# input_wait = browser.find_element(
+#     "xpath",input)
+time.sleep(5)
 print("=============== BERJALAN 4 ==============")
-time.sleep(2)
-input_wait[0].send_keys(text)
-input_wait[0].send_keys(Keys.ENTER)
+input_wait[0].click()
+keyboard.send("enter")
+
 print("=============== FINISH ==============")
